2bc4062693c7b74999b84e02e6dfe642ea3595d0
Eyüp Alemdar <eyupalemdar@outlook.com>
2020-05-31 18:29:21 +0300

    Added chartist files

---
+++ src/BlazorChartist/wwwroot/BlazorChartist.js
@@ -0,0 +1,18 @@
+﻿var BlazorChartist = {
+  createChart: function (elem) {
+    // an example copied from Chartist.js web site
+    new Chartist.Line(elem, {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      series: [
+        [12, 9, 7, 8, 5],
+        [2, 1, 3.5, 7, 3],
+        [1, 3, 4, 5, 6]
+      ]
+    }, {
+      fullWidth: true,
+      chartPadding: {
+        right: 40
+      }
+    });
+  }
+};
