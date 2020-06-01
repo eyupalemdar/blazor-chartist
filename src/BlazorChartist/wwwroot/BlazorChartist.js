1d138d8be0db0f1240dec5d5a4f8d5adebab77aa
Eyüp Alemdar <eyupalemdar@outlook.com>
2020-06-01 13:50:08 +0300

    Chart data types

--- src/BlazorChartist/wwwroot/BlazorChartist.js
+++ src/BlazorChartist/wwwroot/BlazorChartist.js
@@ -1,18 +1,12 @@
 ﻿var BlazorChartist = {
-  createChart: function (elem) {
-    // an example copied from Chartist.js web site
-    new Chartist.Line(elem, {
-      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-      series: [
-        [12, 9, 7, 8, 5],
-        [2, 1, 3.5, 7, 3],
-        [1, 3, 4, 5, 6]
-      ]
-    }, {
-      fullWidth: true,
-      chartPadding: {
-        right: 40
-      }
-    });
+
+  createChart: function (elem, type) {
+    var constructor = Chartist[type];
+    elem.chart = new constructor(elem, { series: [] });
+  },
+
+  updateChart: function (elem, data) {
+    elem.chart.update(data);
   }
+
 };
